7df51c28a2e791229b0436c352f4fc37fb86a5a3
cunananm2000 <cunananm2000@gmail.com>
2022-06-21 15:19:12 +1000

    pc

--- BruteForceSolver.py
+++ BruteForceSolver.py
@@ -1,6 +1,6 @@
 import json
 from utils import filterPossible, getSplits, check
-from wordLists import guesses, answers
+from originalWordLists import guesses, answers
 from tqdm.auto import tqdm
 from valuations import *
 import numpy as np
@@ -41,14 +41,14 @@ class Solver(object):
         elif len(sub) == 2:
             self.best[c] = sub[0]
             ans = 1.5
-        elif depth >= 6:
+        elif depth >= 7:
             self.best[c] = sub[0]
             ans = 1000
             # print("Gone too far")
         else:
             bestScore = -1
             bestGuess = ""
-            for g in tqdm(sorted(G, key=lambda x: mostParts(x, sub))[:10], disable = not (depth < 3)):
+            for g in tqdm(sorted(G, key=lambda x: (mostParts(x, sub),1 - (x in sub)) )[:30], disable = not (depth < 3)):
                 t = 1
                 splits = getSplits(g, sub, useWords = True)
                 if len(splits) == 1: continue
@@ -88,60 +88,142 @@ class Solver(object):
         
 
 if __name__ == "__main__":
-    s = Solver(words = ['again',
- 'amass',
- 'avail',
- 'awash',
- 'bland',
- 'blank',
- 'flail',
- 'flaky',
- 'flank',
- 'flash',
- 'flask',
- 'foamy',
- 'gland',
- 'glass',
- 'gnash',
- 'guava',
- 'khaki',
- 'koala',
- 'llama',
- 'loamy',
- 'piano',
- 'plaid',
- 'plain',
- 'plank',
- 'plaza',
- 'psalm',
- 'quail',
- 'qualm',
- 'quash',
- 'quasi',
- 'shady',
- 'shaky',
- 'shall',
- 'shank',
- 'shawl',
- 'slain',
- 'slang',
- 'slash',
- 'small',
- 'smash',
- 'snail',
- 'snaky',
- 'soapy',
- 'spank',
- 'spasm',
- 'spawn',
- 'swami',
- 'swamp',
- 'swash'])
+#     s = Solver(words = ['abyss',
+#  'admin',
+#  'affix',
+#  'afoul',
+#  'aging',
+#  'aglow',
+#  'agony',
+#  'album',
+#  'alibi',
+#  'align',
+#  'allay',
+#  'allow',
+#  'alloy',
+#  'along',
+#  'aloof',
+#  'aloud',
+#  'alpha',
+#  'amiss',
+#  'among',
+#  'amply',
+#  'annoy',
+#  'annul',
+#  'anvil',
+#  'aphid',
+#  'aping',
+#  'apply',
+#  'assay',
+#  'audio',
+#  'avian',
+#  'avoid',
+#  'awful',
+#  'axial',
+#  'axiom',
+#  'axion',
+#  'badly',
+#  'baggy',
+#  'balmy',
+#  'banal',
+#  'banjo',
+#  'basal',
+#  'basil',
+#  'basin',
+#  'basis',
+#  'bawdy',
+#  'bayou',
+#  'bylaw',
+#  'daddy',
+#  'daily',
+#  'daisy',
+#  'dally',
+#  'dandy',
+#  'dogma',
+#  'fanny',
+#  'fauna',
+#  'final',
+#  'gaily',
+#  'gamma',
+#  'gassy',
+#  'gaudy',
+#  'gawky',
+#  'gayly',
+#  'gonad',
+#  'handy',
+#  'happy',
+#  'human',
+#  'inlay',
+#  'jazzy',
+#  'kappa',
+#  'kayak',
+#  'lanky',
+#  'lasso',
+#  'laugh',
+#  'loyal',
+#  'madam',
+#  'madly',
+#  'mafia',
+#  'magma',
+#  'mambo',
+#  'mamma',
+#  'mammy',
+#  'manga',
+#  'mango',
+#  'mangy',
+#  'mania',
+#  'manly',
+#  'mason',
+#  'maxim',
+#  'modal',
+#  'nanny',
+#  'nasal',
+#  'naval',
+#  'ninja',
+#  'nomad',
+#  'offal',
+#  'paddy',
+#  'pagan',
+#  'palsy',
+#  'pansy',
+#  'papal',
+#  'pizza',
+#  'polka',
+#  'sadly',
+#  'salad',
+#  'sally',
+#  'salon',
+#  'salsa',
+#  'salvo',
+#  'sandy',
+#  'sappy',
+#  'sassy',
+#  'sauna',
+#  'savoy',
+#  'savvy',
+#  'shoal',
+#  'sigma',
+#  'squad',
+#  'usual',
+#  'valid',
+#  'vapid',
+#  'villa',
+#  'viola',
+#  'vodka',
+#  'voila',
+#  'wagon',
+#  'woman',
+#  'zonal'])
+    s = Solver(words = S)
+    # s = Solver(words = ['spasm', 'swami','soapy','swamp'])
     # s = Solver(words = ['boxer', 'homer', 'hover', 'joker', 'mover', 'roger', 'rover'])
     # bin(s.encode(s.words))
     s.solve()
     # pprint(s.genTree())
-    print(json.dumps(s.genTree(), indent=4, sort_keys=True))
+    tree = s.genTree()
+    with open("originalBestTree.json", "w") as f:
+        json.dump(tree, f, sort_keys=True, indent=4)
+    # print(json.dumps(tree, indent=4, sort_keys=True))
     print(s.memo[s.encode(s.words)])
 
     # print(json.dumps(s.memo, indent=4, sort_keys=True))
